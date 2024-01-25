1a7e6bec0a24f0e66187d01418e9e2b6b7883580
arefzaalounii <you@example.comzaaaref@gmail.com>
2024-01-25 22:39:29 +0100

    done

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-document.getElementById('reservation-button').addEventListener('click', function() {
-    alert('Reserving a table...');
-});
+function calculateTotal() {
+    var numPeople = parseInt(document.getElementById('numPeople').value);
+    var totalCost = 10 * numPeople;  
+    var specialRequests = document.getElementById('specialRequests').value;
+    var numRequests = specialRequests.split('\n').length;
+    totalCost += 5 * numRequests;
+
+    document.getElementById('totalCost').textContent = totalCost;
+}
